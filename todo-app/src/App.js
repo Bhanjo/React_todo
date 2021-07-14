68c3f66649ddb2d65af61f671c8695f624dab24a
Bhanjo <hanjo1515@naver.com>
2021-07-14 17:34:20 +0900

    추가: input 삽입 기능 구현

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -23,10 +23,27 @@ const App = () => {
       checked: false,
     },
   ]);
+  // 고유값으로 사용될 id
+  // ref를 통해 화면에 보여줄 필요가 없는 값들을 관리한다
+  const nextId = useRef(4);
+  
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      // concat으로 원본배열은 보존하고 새로운 복사 배열 생성
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 증가
+    },
+    [todos],
+  );
 
   return (
       <TodoTemplate>
-        <TodoInsert />
+        <TodoInsert onInsert={onInsert} />
         <TodoList todos={todos} />
       </TodoTemplate>
     );
