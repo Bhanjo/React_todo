40ecb508c6458dc6fbfd4deb66e43e81d81727fd
Bhanjo <hanjo1515@naver.com>
2021-07-15 16:35:06 +0900

    useReducer를 통한 컴포넌트 최적화

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -15,10 +15,23 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo => todo.id === action.id ? {...todo, checked: !todo.checked} : todo,);
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
 
   // 나중에 추가할 일정 항목에 대한 state
-  const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   // 고유값으로 사용될 id
   // ref를 통해 화면에 보여줄 필요가 없는 값들을 관리한다
@@ -30,22 +43,17 @@ const App = () => {
       text,
       checked: false,
     };
-    // concat으로 원본배열은 보존하고 새로운 복사 배열 생성
-    setTodos(todos => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo});
     nextId.current += 1; // nextId 증가
     }, []);
 
   const onRemove = useCallback(id => {
       // filter를 통해 선택된 값 이외의 배열을 모두 출력
-      setTodos(todos => todos.filter(todo => todo.id !== id));
+      dispatch({ type: 'REMOVE', id});
     }, []);
 
   const onToggle = useCallback(id => {
-      setTodos(todos =>
-        todos.map(todo =>
-          todo.id === id ? {...todo, checked: !todo.checked} : todo,
-          ),
-      );
+      dispatch({ type: 'TOGGLE', id});
     }, []);
 
   return (
